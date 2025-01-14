2c369ce4ee8b441e66a12ae3d23d68a656d9d082
Josh <353351604@educ.dpcdsb.org>
2025-01-14 08:36:03 -0800

    Allow user to enter name for better greeting

--- src/App.java
+++ src/App.java
@@ -1,5 +1,11 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner input  = new Scanner(System.in);
+
+        System.out.print("Enter your name\n> ");
+        String name = input.nextLine();
+        System.out.println("Hello, " + name + "!");
     }
 }
